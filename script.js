dccec74333d1fe61e4d10ccf0b3abbdf94566f3d
CodeRacon <michael.buschmann.86@gmail.com>
2023-10-15 15:40:58 +0200

    added comment-post-section

--- script.js
+++ script.js
@@ -147,7 +147,14 @@ let suggestions = [
   },
 ];
 
-let users = [];
+let users = [
+  {
+    author: '',
+    profileImg: 'img/icons/profile.svg',
+    comment: '',
+    like: 'false',
+  },
+];
 
 const pinboard = document.getElementById('pinboard');
 
@@ -173,7 +180,7 @@ function renderPinboard() {
 
 function renderCard(cardElement) {
   return /*html*/ `
-    <div class="card" id="card">
+        <div class="card" id="card">
           <div class="card-header">
             <div class="card-header--author">
               <img
@@ -225,7 +232,30 @@ function renderCard(cardElement) {
                   d="M178 34c-21 0-39.26 9.47-50 25.34C117.26 43.47 99 34 78 34a60.07 60.07 0 0 0-60 60c0 29.2 18.2 59.59 54.1 90.31a334.68 334.68 0 0 0 53.06 37a6 6 0 0 0 5.68 0a334.68 334.68 0 0 0 53.06-37C219.8 153.59 238 123.2 238 94a60.07 60.07 0 0 0-60-60Zm-50 175.11C111.59 199.64 30 149.72 30 94a48.05 48.05 0 0 1 48-48c20.28 0 37.31 10.83 44.45 28.27a6 6 0 0 0 11.1 0C140.69 56.83 157.72 46 178 46a48.05 48.05 0 0 1 48 48c0 55.72-81.59 105.64-98 115.11Z" />
               </svg>
             </div>
+            
+
           </div>
+
+          <div class="post-container">
+              <div class="input-bar">
+                <input
+                  class="comment-input"
+                  id="comment"
+                  type="text"
+                  placeholder="add a comment..."
+                  autocomplete="off" />
+                <button 
+                  class="post-btn" 
+                  onclick="addPost()" 
+                  type="button">Post
+                </button>
+              </div>
+              
+              <div id="newpost">
+                <!-- innerHTML -->
+              </div>
+            </div>
+
         </div>
   `;
 }
